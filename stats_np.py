df2b63c9e431913f453f61e2a5d5e4d26f8eed2f
Beliavsky <38887928+Beliavsky@users.noreply.github.com>
2024-09-28 19:25:37 -0400

    added functions dict_stats, some_stats

--- stats_np.py
+++ stats_np.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.stats import skew, kurtosis
 
+stats_names = ["mean", "std", "skew", "kurtosis", "min", "max"]
+
 def print_stats(x, fmt_s="%10s", fmt_r="%10.4f", title=None, end=None):
     """
     Prints the mean, standard deviation, skewness, and kurtosis of a 1D NumPy array.
@@ -18,3 +20,18 @@ def print_stats(x, fmt_s="%10s", fmt_r="%10.4f", title=None, end=None):
         print(fmt_s%key, fmt_r%value)
     if end:
         print(end=end)
+
+def dict_stats(x, fmt_s="%10s", fmt_r="%10.4f", title=None, end=None):
+    """
+    Parameters:
+    - x: ndarray, the input array.
+
+    Returns:
+    - dict of stats
+    """
+    return {"mean":np.mean(x), "sd":np.std(x, ddof=1), "skew":skew(x),
+        "kurtosis":kurtosis(x), "min":np.min(x), "max":np.max(x)}
+
+def some_stats(x):
+    return [np.mean(x), np.std(x, ddof=1), skew(x), kurtosis(x),
+        np.min(x), np.max(x)]
