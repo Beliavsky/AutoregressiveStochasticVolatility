07c2977606b4b4efd0fe95728350b2d21273feb5
Beliavsky <38887928+Beliavsky@users.noreply.github.com>
2024-09-28 19:23:55 -0400

    stores statistics on returns and volatility in a dataframe

--- xarsv.py
+++ xarsv.py
@@ -1,16 +1,23 @@
+""" Simulate from a stochastic volatility model and fit an SV model to the data """
 import numpy as np
+import pandas as pd
 from arsv import arsv_vol_and_returns, fit_arsv
-from stats_np import print_stats
+from stats_np import print_stats, stats_names, some_stats
+from util import print_vec
 
+fmt_r = "%10.6f"
 mu = -0.5       # Long-term mean of log volatility
 phi = 0.9       # Autoregressive parameter
 sigma_v = 0.4   # Volatility of volatility
-nobs = 10**6    # # of returns simulated
+nobs = 10**4    # # of returns simulated
+niter = 3       # # of sets of returns to simulate
 
-x = arsv_vol_and_returns(mu, phi, sigma_v, nobs)
-print_stats(x[:, 0], title="vol", end="\n")
-print_stats(np.log(x[:, 0]), title="log(vol)", end="\n")
-print_stats(x[:, 1], title="returns", end="\n")
-print_stats(x[:, 1]/x[:, 0], title="returns/vol", end="\n")
-print(fit_arsv(x[:, 1]))
-
+for iter in range(niter):
+    x = arsv_vol_and_returns(mu, phi, sigma_v, nobs)
+    print_vec(["mu", "phi", "sigma"], "%10s", label=4*" ")
+    print_vec([mu, phi, sigma_v], fmt_r, label="true")
+    print_vec(fit_arsv(x[:, 1]), fmt_r, label=" fit", end="\n")
+    df = pd.DataFrame({"vol":some_stats(x[:, 0]), "log(vol)":some_stats(np.log(x[:, 0])),
+        "returns":some_stats(x[:, 1]), "returns/vol":some_stats(x[:, 1]/x[:, 0])},
+        index=stats_names).T
+    print(df, end="\n\n")
